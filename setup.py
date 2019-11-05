7941dcfc01232872cfd8d471fe614818bffe0bf7
Alfa Q <alfakyuu@gmail.com>
2019-11-05 00:01:25 -0700

    Version 1.2

--- setup.py
+++ setup.py
@@ -1,29 +1,31 @@
 """Setup package for py-pi"""
 
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
-  name='python-nozomi',
-  packages=['nozomi'],
-  version='1.0',
-  license='MIT',
-  description='Nozomi API for retrieving images, videos, gifs.',
-  author='Alfa_Q',
-  author_email='alfakyuu@gmail.com',
-  url='https://github.com/Alfa-Q/python-nozomi',
-  download_url='https://github.com/Alfa-Q/python-nozomi/archive/1.0.tar.gz',
-  keywords=['nozomi', 'nozomi.la', 'api', 'video', 'image', 'anime'],
-  install_requires=[
-          'requests',
-          'dacite',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-  ],
+    name='python-nozomi',
+    packages=['nozomi'],
+    version='1.2',
+    license='MIT',
+    description='Nozomi API for retrieving images, videos, gifs.',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author='Alfa_Q',
+    author_email='alfakyuu@gmail.com',
+    url='https://github.com/Alfa-Q/python-nozomi',
+    download_url='https://github.com/Alfa-Q/python-nozomi/archive/1.1.tar.gz',
+    keywords=['nozomi', 'nozomi.la', 'api', 'video', 'image', 'anime'],
+    install_requires=[
+        'requests',
+        'dacite',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
