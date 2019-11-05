c6c8b830263da9800aae77c3bac8daf1f391d433
Alfa Q <alfakyuu@gmail.com>
2019-11-04 19:18:36 -0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
   name='python-nozomi',
-  packages=['python-nozomi'],
+  packages=['nozomi'],
   version='1.0',
   license='MIT',
   description='Nozomi API for retrieving images, videos, gifs.',
