3da54ea21077e45dafa91e084cf764c11f9ab2e1
josh / <Joshrmatsushima@hotmail.com>
2020-12-14 13:26:21 -0800

    Add support to parse post ID from URL
    This will make it easier to implement functionality where you can download a single post provided that you have the URL.
    
    This feature will also make it easier to test specific posts (and resolve issue #7).

--- nozomi/exceptions.py
+++ nozomi/exceptions.py
@@ -5,3 +5,6 @@ class NozomiException(Exception):
 
 class InvalidTagFormat(NozomiException):
     """The tag is not in valid format (i.e. empty string)."""
+
+class InvalidUrlFormat(NozomiException):
+    """The url is not in valid format."""
